c4d6958e999f681420c0fa19032da21556a5dd1f
Nico Salate Santone <57440475+nsalatesantone@users.noreply.github.com>
2026-07-16 16:27:02 +0200

    fix

--- app-babyskull/flutter_bootstrap.js
+++ app-babyskull/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1122707702" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3201975931" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
